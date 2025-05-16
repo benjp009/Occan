07ea4f6454135a88f2ba70cada97009318ba5c1d
Benjamin Patin <patin.benjamin@gmail.com>
2025-05-16 13:42:18 +0200

    update cards design - unsuccessful

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,15 +3,15 @@ import { CompanyRow } from '../types';
 
 export function Card({ company }: { company: CompanyRow }) {
   return (
-    <div className="border p-4 rounded flex flex-col items-center">
+    <div className="w-full sm:w-64 md:w-56 lg:w-60 border p-4 rounded flex flex-col items-center">
       {/* Company Logo */}
       {company.logo && (
         <div className="h-10 w-10 mb-4 flex-shrink-0 overflow-hidden">
           <img
             src={company.logo}
             alt={`${company.name} logo`}
-            width={60}
-            height={60}
+            width={80}
+            height={80}
             style={{ objectFit: 'contain' }}
           />
         </div>
