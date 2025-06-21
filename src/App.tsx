8697966749da213d007581027db346919cc0c4aa
Benjamin Patin <patin.benjamin@gmail.com>
2025-06-21 23:39:22 +0200

    Adding search page and search redirect

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Software from './pages/Software';
 import AddSoftware from './pages/AddSoftware';
 import AllCategory from './pages/AllCategory';
 import Category from './pages/Category';
+import SearchResults from './pages/SearchResults';
 import LLMInjection from './components/LLMInjection';
 
 
@@ -26,6 +27,7 @@ export default function App() {
         <Route path="/toutes-categories" element={<AllCategory />} />
         <Route path="/categorie/:slug" element={<Category />} />
         <Route path="/logiciel/:slug" element={<Software />} />
+        <Route path="/recherche" element={<SearchResults />} />
         {/* …other routes */}
       </Routes>
     </Router>
