3e54ae2220826fad19262895ef91f8e1e4295d4d
Benjamin Patin <patin.benjamin@gmail.com>
2025-06-25 14:40:59 +0200

    Contact and a propos page

--- src/pages/APropos.tsx
+++ src/pages/APropos.tsx
@@ -15,14 +15,19 @@ export default function APropos() {
             sur le territoire afin de faciliter la mise en relation entre éditeurs et utilisateurs.
           </p>
           <p>
-            Le projet est né d\'une volonté de mettre en avant le savoir‑faire
-            français et de favoriser l\'écosystème local. Toutes les données
-            présentées proviennent d\'une feuille Google Sheets ouverte à la
-            communauté et mise à jour régulièrement.
+            Le projet est né d'une volonté de mettre en avant le savoir‑faire
+            français et de favoriser l'écosystème local. Toutes les données
+            présentées proviennent d'une base de donnée mise à jour quotidiennement par nos équipes. 
           </p>
           <p>
-            N\'hésitez pas à nous contacter si vous souhaitez référencer votre logiciel
-            ou contribuer à l\'amélioration du site.
+            Nous sélections uniquement des logiciels développés par des sociétés dont le siège social est basé en France. 
+            Notre volonté n’est pas de mettre en avant un logiciel plutôt qu’un autre mais bien de mettre à disposition une base complète de logiciels made in France afin que vous puissiez identifier les solutions qui correspondent au mieux à vos besoins.
+            N’hésitez pas à contribuer au projet en nous partageant des solutions françaises que vous aimez et utilisez régulièrement !
+          </p>
+          <a href="/ajouter-un-nouveau-logiciel"> Ajouter un nouveau logiciel </a>
+          <p>
+            N'hésitez pas à nous <a href="/contact">contacter</a> si vous souhaitez référencer votre logiciel
+            ou contribuer à l'amélioration du site.
           </p>
         </section>
       </main>
