7ab94115a8ad812dcef8a480fd225ef269627280
Benjamin Patin <patin.benjamin@gmail.com>
2025-07-03 15:51:41 +0200

    meta description for software pages

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ tsNode.register({
   require.extensions[ext] = () => '';
 });
 const App = require('./src/App.tsx').default;
+const { HelmetProvider } = require('react-helmet-async');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 const indexFile = path.resolve('./build/index.html');
+app.use(express.static(path.resolve('./build')));
 app.use('/static', express.static(path.resolve('./build/static')));
 app.use('/asset', express.static(path.resolve('./build/asset')));
 
@@ -25,11 +27,19 @@ app.get('*', (req, res) => {
     if (err) {
       return res.status(500).send('Error loading HTML file');
     }
+    const helmetContext = {};
     const appHtml = ReactDOMServer.renderToString(
-      React.createElement(App, { location: req.url })
+      React.createElement(
+        HelmetProvider,
+        { context: helmetContext },
+        React.createElement(App, { location: req.url })
+      )
     );
+    const { helmet } = helmetContext;
     const finalHtml = htmlData.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
-    return res.send(finalHtml);
+    const helmetString = helmet ? `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}` : '';
+    const htmlWithHelmet = finalHtml.replace('</head>', `${helmetString}</head>`);
+    return res.send(htmlWithHelmet);
   });
 });
 
