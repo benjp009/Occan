ec57b5593d47f5daedda42969a9161373049b553
Benjamin Patin <patin.benjamin@gmail.com>
2025-07-03 16:42:44 +0200

    push server.js

--- server.js
+++ server.js
@@ -35,11 +35,23 @@ app.get('*', (req, res) => {
         React.createElement(App, { location: req.url })
       )
     );
-    const { helmet } = helmetContext;
-    const finalHtml = htmlData.replace('<div id="root"></div>', `<div id="root">${appHtml}</div>`);
-    const helmetString = helmet ? `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}` : '';
-    const htmlWithHelmet = finalHtml.replace('</head>', `${helmetString}</head>`);
-    return res.send(htmlWithHelmet);
+          const { helmet } = helmetContext;
+
+      // Remove the default meta description if one is provided by Helmet
+      let htmlWithoutDefaultMeta = htmlData;
+      if (helmet && helmet.meta && helmet.meta.toString().includes('name="description"')) {
+        htmlWithoutDefaultMeta = htmlData.replace(/<meta\s+name="description"[^>]*>/i, '');
+      }
+
+      const finalHtml = htmlWithoutDefaultMeta.replace(
+        '<div id="root"></div>',
+        `<div id="root">${appHtml}</div>`
+      );
+      const helmetString = helmet
+        ? `${helmet.title.toString()}${helmet.meta.toString()}${helmet.link.toString()}`
+        : '';
+      const htmlWithHelmet = finalHtml.replace('</head>', `${helmetString}</head>`);
+      return res.send(htmlWithHelmet);
   });
 });
 
